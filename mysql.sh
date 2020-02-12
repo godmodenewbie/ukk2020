70746783f73a40bc6ed792f167c0234cb67d47ed
Nobin Dwi Adi Saputra <edwardmiles91@gmail.com>
2020-02-12 18:41:29 +0800

    CUMAN NAMBAHIN WARNA AJA WKWK

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,12 @@
-echo "pastiin situ dah masuk ke mode root pak ! biar ga kena error permission denied"
-echo "inget pas muncul tampilan biru pilih "ok" baru enter"
+bold=$(tput bold)
+normal=$(tput normal)
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo "${bold}${red}pastiin situ dah masuk ke mode root pak ! biar ga kena error permission denied"
+echo "${bold}${red}inget pas muncul tampilan biru pilih "ok" baru enter"
+echo "${reset}LAGI NGINSTAL PAK"
 
 apt update 
 apt install gnupg sudo links wget curl
@@ -7,4 +14,5 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
 dpkg -i mysql-apt-config_0.8.13-1_all.deb
 apt update
 apt install -y mysql-server
-echo "selesaii"
+echo "${bold}${green} SELESAI"
+${reset}
